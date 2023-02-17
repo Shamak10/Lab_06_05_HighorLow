5895abfb5fb144c5a95611b4d93bd68ce78f0fc3
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-17 17:21:45 -0500

    Update guessCalculator.java

--- src/guessCalculator.java
+++ src/guessCalculator.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class guessCalculator {
+    public static void main(String[] args) {//Creating scanner to take input from the user
+        Scanner scanner = new Scanner("7");//Asking for the user input
+        int randomNumber = 7;
+        System.out.print("Guess a number between 1 and 10: ");//choose a number between 1 and 10
+        int userGuess = scanner.nextInt();
+        System.out.println("The random number was: " + randomNumber);//The value of random number
+        if (userGuess == randomNumber) {
+            System.out.println("Your guess was on the money!");//The number guessed was right
+        } else if (userGuess > randomNumber) {
+            System.out.println("Your guess was too high!");//The number guessed was high
+        } else {
+            System.out.println("Your guess was too low!");//The number guessed was low
+        }
     }
 }
